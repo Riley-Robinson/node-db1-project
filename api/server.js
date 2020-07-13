5b9c253ea3cb647d102ac5595af9847c526526ac
Riley <rrtoinfinity@gmail.com>
2020-07-13 17:48:12 -0400

    mvp i think

--- api/server.js
+++ api/server.js
@@ -4,6 +4,19 @@ const db = require("../data/dbConfig.js");
 
 const server = express();
 
+const router = require("../router");
+
 server.use(express.json());
+
+server.use("/api/accounts", logger, router);
+
+server.get("/", (req, res) => {
+    res.send(`<h1> Were Live Love</h1>`);
+})
+
+function logger(req, res, next) {
+    console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+    next();
+}
 
 module.exports = server;
