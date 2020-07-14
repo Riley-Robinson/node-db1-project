344861126769df975c2f06e9c4e6fd1bd7de6521
Riley <rrtoinfinity@gmail.com>
2020-07-14 16:33:20 -0400

    i fixed

--- api/server.js
+++ api/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const db = require("../data/dbConfig.js");
 
 const server = express();
@@ -8,15 +7,16 @@ const router = require("../router");
 
 server.use(express.json());
 
+
 server.use("/api/accounts", logger, router);
 
 server.get("/", (req, res) => {
-    res.send(`<h1> Were Live Love</h1>`);
-})
+    res.send(`<h2>This is the message for a general server get request</h2>`);
+});
 
 function logger(req, res, next) {
     console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
     next();
-}
-
-module.exports = server;
+  }
+  
+  module.exports = server; 
